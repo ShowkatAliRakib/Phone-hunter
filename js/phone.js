67ea37cca5ae846c49eaea8b36fd99cd1737586b
ShowkatAliRakib <rakibmd54496313@gmail.com>
2022-03-01 01:02:38 +0600

    added-js-code

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,25 @@
-console.log(" find phone");
+const searchbtn = () => {
+
+    const input = document.getElementById("input-value");
+    const inputValue = input.value;
+
+
+
+
+
+
+
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
+        .then(res => res.json())
+        .then(data => phoneDisplay(data))
+
+
+}
+
+
+
+
+
+const phoneDisplay = (data) => {
+    console.log(data);
+}
